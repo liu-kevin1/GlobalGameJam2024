790a30c6a807fd6d887cd4a0322cd5af362e8375
orgwirelist123 <orgwirelist123@gmail.com>
2024-01-27 16:40:30 -0500

    added Spaghetti character

---
+++ Assets/Scripts/Data/Characters/Spaghetti.gd
@@ -0,0 +1,17 @@
+class_name Cheesecake
+extends Character
+
+var character : Character 
+
+func _init():
+	var sprites = {
+		IDLE = {
+            Texture = preload("res://Assets/Images/Squid_Ink_Spaget.png"),
+            Scale = Vector2(0.193, 0.184),
+            Position = Vector2(1147, 776)
+        }
+	}
+	character = Character.new("Spaghetti", sprites)
+
+func testCharacterFunction():
+	print("TEST CHARACTER - TEST CHARACTER FUNCTION")
