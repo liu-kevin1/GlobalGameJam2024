6def439bb05bd2ac5391b6af141e3181868f962a
Hanna Nakajima <hnakajima238@gmail.com>
2024-01-28 10:44:13 -0500

    Edited prime rib placement and scale.

--- Scripts/Data/Characters/PrimeRib.gd
+++ Scripts/Data/Characters/PrimeRib.gd
@@ -7,8 +7,8 @@ func _init():
 	var sprites = {
 		IDLE = {
 			Texture = preload("res://Assets/Images/Prime_Rib.png"),
-			Scale = Vector2(0.2, 0.2),
-			Position = Vector2(1147, 776)
+			Scale = Vector2(0.7, 0.7),
+			Position = Vector2(1147, 820)
 		}
 	}
 	character = Character.new("PrimeRib", sprites)
