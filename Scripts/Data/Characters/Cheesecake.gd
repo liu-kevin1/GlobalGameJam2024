bc4fe0cc9a6b5c567a2247779aa1ded81604b9b5
orgwirelist123 <orgwirelist123@gmail.com>
2024-11-09 16:43:33 -0500

    fixed textures for Cheesecake and RockCandy

--- Scripts/Data/Characters/Cheesecake.gd
+++ Scripts/Data/Characters/Cheesecake.gd
@@ -11,7 +11,8 @@ func _init():
 			Position = Vector2(1147, 876)
 		},
 		BLINK = {
-			Texture = preload("res://Assets/Images/shinji_chair.png"),
+			Texture = preload("res://Assets/Images/Mashed-Up_Cheesecake.png"),
+			# Texture = preload("res://Assets/Images/shinji_chair.png"),
 			Scale = Vector2(0.5, 0.5),
 			Position = Vector2(1147, 876),
 			Frequency = 0.5, # 0.5 -> 50% chance each second for this sprite to blink
@@ -19,7 +20,8 @@ func _init():
 			Wait = 0.75 # 0.75 -> Waits 0.75 seconds between each attempt to blink
 		},
 		FLUSHED = {
-			Texture = preload("res://Assets/Images/jerma_july.jpg"),
+			Texture = preload("res://Assets/Images/Mashed-Up_Cheesecake.png"),
+			# Texture = preload("res://Assets/Images/jerma_july.jpg"),
 			Scale = Vector2(0.5, 0.5),
 			Position = Vector2(1147, 876)
 		}
--- Scripts/Data/Characters/RockCandy.gd
+++ Scripts/Data/Characters/RockCandy.gd
@@ -6,7 +6,7 @@ var character : Character
 func _init():
 	var sprites = {
 		IDLE = {
-			Texture = preload("res://Assets/Images/Steak.png"),
+			Texture = preload("res://Assets/Images/Rock_Candy.png"),
 			Scale = Vector2(0.193, 0.184),
 			Position = Vector2(1147, 776)
 		}
