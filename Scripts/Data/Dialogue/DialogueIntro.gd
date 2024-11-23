fd01fc29b34eab1e9581d3164b789777df5e515b
Hanna Nakajima <hnakajima238@gmail.com>
2024-11-22 23:32:52 -0500

    Intro Scene Typo 'dinning' --> 'dining'

--- Scripts/Data/Dialogue/DialogueIntro.gd
+++ Scripts/Data/Dialogue/DialogueIntro.gd
@@ -34,11 +34,11 @@ func _init():
 	dialogueManager = DialogueManager.instance
 	# print("Initialized DialogueCheesecake")
 	dialogueManager.addDialogue(Dialogue.new("IntroScene", [
-        DialogueSwitchBackground.new("IntroScene"),
+		DialogueSwitchBackground.new("IntroScene"),
 		DialogueSwitchSprite.new("Player"),
 		DialogueLine.new("Player", "...?"),
 		DialogueLine.new("Player", "Free food... I guess it wouldn't hurt to go..... right?"),
-        DialogueSwitchBackground.new("DiningTableBackground"),
+		DialogueSwitchBackground.new("DiningTableBackground"),
 		DialogueSwitchSprite.new("Narrator"),
 		DialogueLine.new("Narrator", "You enter a wide, open room. It’s dimly lit with candles at each table."),
 		DialogueLine.new("Narrator", "The server brings you to a booth seat sectioned off by elegant red curtains."),
@@ -46,10 +46,10 @@ func _init():
 		DialogueSwitchSprite.new("Server"),
 		DialogueLine.new("Server", "Here you are, my esteemed guest. As you are aware, tonight is the grand opening of Meat N’ Eet."),
 		DialogueLine.new("Server", "We are a new high-end restaurant whom prides itself in top-quality service and a unique experience unlike any other."),
-		DialogueLine.new("Server", "As you know, there is a set 6 course meal to pair with your dinning experience."),
+		DialogueLine.new("Server", "As you know, there is a set 6 course meal to pair with your dining experience."),
 		DialogueLine.new("Server", "The first course will be out shortly. Please enjoy."),
-		DialogueSwitchSprite.new("Narrator"),
-		DialogueLine.new("Narrator", "You sit there. Taking in the pleasant atmosphere of the dinning room."),
+		
+		DialogueLine.new("Narrator", "You sit there. Taking in the pleasant atmosphere of the dining room."),
 		DialogueLine.new("Narrator", "You haven’t left your room in eons."),
 		DialogueLine.new("Narrator", "Every once in a while it’s good to get out..."),
 		DialogueLine.new("Narrator", "Speaking of which, isn’t there supposed to be a date..."),
