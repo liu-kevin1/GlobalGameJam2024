e8f45f2132cc71e459522e723f790d1e93faf140
Hanna Nakajima <hnakajima238@gmail.com>
2024-11-23 14:06:24 -0500

    Fixed Dialogue: Sashimi replaced with sushi in the dialogue.

--- Scripts/Data/Dialogue/DialogueSashimi.gd
+++ Scripts/Data/Dialogue/DialogueSashimi.gd
@@ -11,10 +11,10 @@ func _init():
 		DialogueLine.new("Server", "Behold, our only and best selection of raw fish"),
 		DialogueLine.new("Server", "Hopefully the addons are to your liking."),
 		DialogueLine.new("Server", "We went through heaven and hell collecting the finest ingredients"),
-		DialogueLine.new("Server", "We hope you will enjoy this delectable dish of sashimi."),
+		DialogueLine.new("Server", "We hope you will enjoy this delectable dish of sushi."),
 		DialogueSwitchSprite.new("Sashimi"),
 		DialogueLine.new("Sashimi", "Oh its you . . . As they say, the day will always return."),
-		DialogueLine.new("Player", "Huh, I have never met you nor eaten Sashimi before."),
+		DialogueLine.new("Player", "Huh, I have never met you nor eaten sushi before."),
 		DialogueSwitchSprite.new("Sashimi", "HAPPY"),
 		DialogueLine.new("Sashimi", "Well you haven't lived a good life haven't you?"),
 		DialogueLine.new("Player", "What?"),
@@ -31,7 +31,7 @@ func _init():
 		DialogueLine.new("Sashimi", "Many say after WWIII, humanity will come together, if they still exist, and prosper beyond our current visions. However, just looking at you, all I see is history repeating itself again."),
 		DialogueLine.new("Player", "Whoa a bit dark reall-"),
 		DialogueLine.new("Sashimi", "And you are just going to forget after consuming. Pleasure, such a poison to humans. Never ending hunger for more. What will you do after consuming me? Will you remember me? In fact, how much do you even remember about your  past? Probably miniscule to the amount you've consumed.", DialogueLineModifiers.new(1, true, [
-			Option.new("Eat the sashimi to make it shut up", func(): dialogueManager.playDialogue("Sashimi_ShinjiChairMoment")),
+			Option.new("Eat the sushi to make it shut up", func(): dialogueManager.playDialogue("Sashimi_ShinjiChairMoment")),
 			Option.new("Ponder on the said statements", func(): dialogueManager.playDialogue("Sashimi_Ponder")),
 			Option.new("That's not true...", func(): dialogueManager.playDialogue("Sashimi_Love"))
 		])),
@@ -67,7 +67,7 @@ func _init():
 		DialogueSwitchSprite.new("Sashimi", "FLUSHED"),
 		DialogueLine.new("Sashimi", "O-oh does it now?"),
 		DialogueLine.new("Player", "Yeah it makes you incredibly appealing", DialogueLineModifiers.new(1, true, [
-			Option.new("I might just take you home...", func(): dialogueManager.enterCredits("[ YOU LOVE SASHIMI >~< ]")),
+			Option.new("I might just take you home...", func(): dialogueManager.enterCredits("[ YOU LOVE SUSHI >~< ]")),
 			Option.new("Not appealing enough though", func(): dialogueManager.playDialogue("Sashimi_Skip"))
 		])),
 	]))
@@ -82,7 +82,7 @@ func _init():
 		DialogueLine.new("Player", "Please rid me of this guy."),
 		DialogueSwitchSprite.new("Server"),
 		DialogueLine.new("Server", "I'll collect that, then."),
-		DialogueLine.new("Narrator", "The server comes over to retrieve the dish, heading back into the kitchen.")
+		DialogueLine.new("Narrator", "The server comes over to retrieve the dish, heading back into the kitchen."),
 		DialogueLine.new("Player", ".........", DialogueLineModifiers.new(1, true, [], [
 			func(): dialogueManager.playDialogue("PrimeRib_Served")
 		]))
@@ -91,7 +91,7 @@ func _init():
 		DialogueSwitchSprite.new("Sashimi", "FLUSHED"),
 		DialogueLine.new("Sashimi", "What type of question is that? Of course I'm fine"),
 		DialogueLine.new("Player", "Quite the pessimistic worldview don't you think."),
-		DialogueLine.new("Sashimi", "Well, when you're talking sashimi, what other options do I have?"),
+		DialogueLine.new("Sashimi", "Well, when you're talking sushi, what other options do I have?"),
 		DialogueLine.new("Player", "..."),
 		DialogueSwitchSprite.new("Sashimi", "HAPPY"),
 		DialogueLine.new("Sashimi", "Look, consumption and short term enjoyments are ok. I just get pissed looking at the state of life."),
