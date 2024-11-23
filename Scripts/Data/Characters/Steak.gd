76efa97a6877f063029f06db1909563241905b76
orgwirelist123 <orgwirelist123@gmail.com>
2024-11-22 19:57:54 -0500

    fixed location of sprites and plates

--- Scripts/Data/Characters/Steak.gd
+++ Scripts/Data/Characters/Steak.gd
@@ -8,13 +8,13 @@ func _init():
 		IDLE = {
 			Texture = preload("res://Assets/Images/CharacterSprites/Steak/steak.png"),
 			Scale = Vector2(0.6, 0.6),
-			Position = Vector2(1174, 731)
+			Position = Vector2(1160, 749)
 		},
 		BLINK = {
 			Texture = preload("res://Assets/Images/CharacterSprites/Steak/steak_blink.png"),
 			# Texture = preload("res://Assets/Images/shinji_chair.png"),
 			Scale = Vector2(0.6, 0.6),
-			Position = Vector2(1174, 731),
+			Position = Vector2(1160, 749),
 			Frequency = 0.5, # 0.5 -> 50% chance each second for this sprite to blink
 			Duration = 0.5, # 0.5 -> Sprite stays on blink sprite for 0.5 seconds before going back to idle
 			Wait = 0.75 # 0.75 -> Waits 0.75 seconds between each attempt to blink
@@ -23,25 +23,25 @@ func _init():
 			Texture = preload("res://Assets/Images/CharacterSprites/Steak/steak_confused.png"),
 			# Texture = preload("res://Assets/Images/jerma_july.jpg"),
 			Scale = Vector2(0.6, 0.6),
-			Position = Vector2(1174, 731)
+			Position = Vector2(1160, 749)
 		},
 		DISGUSTED = {
 			Texture = preload("res://Assets/Images/CharacterSprites/Steak/steak_disgusted.png"),
 			# Texture = preload("res://Assets/Images/jerma_july.jpg"),
 			Scale = Vector2(0.6, 0.6),
-			Position = Vector2(1174, 731)
+			Position = Vector2(1160, 749)
 		},
 		FLUSHED = {
 			Texture = preload("res://Assets/Images/CharacterSprites/Steak/steak_flushed.png"),
 			# Texture = preload("res://Assets/Images/jerma_july.jpg"),
 			Scale = Vector2(0.6, 0.6),
-			Position = Vector2(1174, 731)
+			Position = Vector2(1160, 749)
 		},
 		HAPPY = {
 			Texture = preload("res://Assets/Images/CharacterSprites/Steak/steak_happy.png"),
 			# Texture = preload("res://Assets/Images/jerma_july.jpg"),
 			Scale = Vector2(0.6, 0.6),
-			Position = Vector2(1174, 731)
+			Position = Vector2(1160, 749)
 		},
 		PLATE = {
 			Texture = preload("res://Assets/Images/Plate.png")
